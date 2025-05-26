7c4c7bbdd9c9532e69a83639ea3a94be6ed92399
paskalev2001 <2001paskalpaskalev@gmail.com>
2025-05-26 17:50:39 +0300

    Initial commit

---
+++ platformer/Python-Platformer-main/menu.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import game
+import pygame
+WIDTH = 640
+HEIGHT = 480
+game_window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+game.main()
+# window = Tk()
+# window.title("Main Menu - Platformer")
+# window.mainloop()
